e4c2cbb4a9b3a7d01e90e9cebb024e8a3a7fa53f
KMoussa33 <kelsey.moussa@gmail.com>
2023-01-25 12:46:47 -0500

    Redux campsitesSlice

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -20,71 +20,71 @@ const CommentForm = ({ campsiteId }) => {
         <>
             <Button outline onClick={() => setModalOpen(true)}>
                 <i className='fa fa-pencil fa-lg' /> Add Comment
-                <Modal isOpen={modalOpen}>
-                    <ModalHeader toggle={() => setModalOpen(false)}>
-                        Add Comment
-                    </ModalHeader>
-                    <ModalBody>
-                        <Formik initialValues={{
-                            rating: undefined,
-                            author: '',
-                            commentText: ''
-                        }}
-                        onSubmit={handleSubmit}
-                        validate={validateCommentForm}
-                        >
-                            <Form>
-                                <FormGroup>
-                                    <Label htmlFor='rating'>
-                                        Rating
-                                    </Label>
-                                    <Field
-                                        name='rating'
-                                        as='select'
-                                        className='form-control'
-                                    >
-                                        <option>Select...</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Field>
-                                    <ErrorMessage name='rating'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor='author'>
-                                        Your Name
-                                    </Label>
-                                    <Field
-                                        name='author'
-                                        placeholder='Your Name'
-                                        className='form-control'
-                                    />
-                                    <ErrorMessage name='author'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label htmlFor='commentText'>
-                                        Comment
-                                    </Label>
-                                    <Field
-                                        name='commentText'
-                                        as='textarea'
-                                        rows='12'
-                                        className='form-control'
-                                    />
-                                </FormGroup>
-                                <Button type='submit' color='primary'>
-                                    Submit
-                                </Button>
-                            </Form>
-                        </Formik>
-                    </ModalBody>
-                </Modal>
+            <Modal isOpen={modalOpen}>
+                <ModalHeader toggle={() => setModalOpen(false)}>
+                    Add Comment
+                </ModalHeader>
+                <ModalBody>
+                    <Formik initialValues={{
+                        rating: undefined,
+                        author: '',
+                        commentText: ''
+                    }}
+                    onSubmit={handleSubmit}
+                    validate={validateCommentForm}
+                    >
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor='rating'>
+                                    Rating
+                                </Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                                <ErrorMessage name='rating'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>
+                                    Your Name
+                                </Label>
+                                <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                />
+                                <ErrorMessage name='author'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='commentText'>
+                                    Comment
+                                </Label>
+                                <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
+                </ModalBody>
+            </Modal>
             </Button>
         </>
     );
