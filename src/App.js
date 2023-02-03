d3c3a0a89ab735a112fb465e48f0d5da7e110aba
KMoussa33 <kelsey.moussa@gmail.com>
2023-02-03 11:25:41 -0500

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
+
 
 
 
@@ -21,6 +23,7 @@ function App() {
   useEffect(() => {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
+    dispatch(fetchPromotions());
   }, [dispatch]);
 
   return (
